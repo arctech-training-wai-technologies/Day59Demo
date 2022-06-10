32c2f2a69a438d7ae26be0aedf1a3b25c439b193
Avinash Tauro <avinash.tauro@arctechinfo.com>
2022-06-10 18:34:38 +0530

    Validation Client Side

---
+++ Day59DemoValidations/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using Day59DemoValidations.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Day59DemoValidations.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(User user)
+        {
+            // Save to database
+            return View();
+        }
+    }
+}
---
+++ Day59DemoValidations/Models/Services/ValidationAttributes/MinAge.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Day59DemoValidations.Models.Services.ValidationAttributes;
+
+public class MinAge : ValidationAttribute
+{
+    private int _Limit;
+    public MinAge(int Limit)
+    { // The constructor which we use in modal.
+        this._Limit = Limit;
+    }
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+        DateTime bday = DateTime.Parse(value.ToString());
+        DateTime today = DateTime.Today;
+        int age = today.Year - bday.Year;
+        if (bday > today.AddYears(-age))
+        {
+            age--;
+        }
+        if (age < _Limit)
+        {
+            var result = new ValidationResult("Sorry you are not old enough");
+            return result;
+        }
+
+
+        return null;
+
+    }
+}
